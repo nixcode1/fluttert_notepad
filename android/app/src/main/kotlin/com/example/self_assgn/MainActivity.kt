f95e57d26ceef03d521c24203a4f0d6cdc0de961
Chukwuebuka Amaechi <amaechi.chukwuebuka1@>
2020-03-31 22:14:35 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/self_assgn/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.self_assgn
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
